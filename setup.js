f81c81a176c5fac91e7a66bff60a21954e123fa7
hell12335 <66856905+hell12335@users.noreply.github.com>
2020-12-18 20:19:33 -0300

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlcarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
